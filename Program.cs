c8ff07a6165f98ec16ec174556bddc4de5b6168f
jimmybengtsson <jb223pu@student.lnu.se>
2017-03-27 18:22:30 +0200

    Added repo and a cs file.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jb223pu_examination_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
