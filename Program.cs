2d3e5ddb7ae159dc3a872ee56168ad0ff3aaed3f
jimmybengtsson <jb223pu@student.lnu.se>
2017-03-28 18:00:17 +0200

    Added Median and range.

--- Program.cs
+++ Program.cs
@@ -15,6 +15,9 @@ namespace jb223pu_examination_1
             
             Console.WriteLine(Maximum(jsonArray));
             Console.WriteLine(Minimum(jsonArray));
+            Console.WriteLine(Mean(jsonArray));
+            Console.WriteLine(Median(jsonArray));
+            Console.WriteLine(Range(jsonArray));
         }
 
         static int Maximum(int[] source)
@@ -27,6 +30,36 @@ namespace jb223pu_examination_1
             {
                 int minValue = source.Min();
                 return minValue;
+            }
+
+        static double Mean(int[] source)
+            {
+                double mean = source.Average();
+                return mean;
+            }
+
+        static double Median(int[] source)
+            {
+                int[] temp = (int[])source.Clone();    
+                Array.Sort(temp);
+                
+                
+
+                if (temp.Length % 2 == 0)
+                {
+                    int a = temp[temp.Length / 2 -1];
+                    int b = temp[temp.Length / 2];
+                    return (a + b);
+                }
+                else
+                {
+                    return temp[temp.Length / 2];
+                }
+            }
+
+        static int Range(int[] source)
+            {
+                return source.Max() - source.Min();
             }
     }
 }
